72323eeed5f037f2117ebc85dbb626191e618d32
pritishraj01 <pritish499@gmail.com>
2025-09-27 16:14:19 +0530

    improving

--- index.js
+++ index.js
@@ -8,10 +8,15 @@ import cors from "cors"
 
 const app = express()
 const port = process.env.PORT || 4000
+const allowedOrigins = [
+  "https://chatgpt-frontend-eta.vercel.app",
+  "http://localhost:5173" // for local dev
+]
 
 app.use(express.json())
 app.use(cors({
     "origin":"https://chatgpt-frontend-eta.vercel.app",
+
     credentials:true
 }))
 app.use(cookieParser())
